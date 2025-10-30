caeafe2acd4682bd029c89d71e20a26f02c765b4
bbhuri <bhurikanpai1447@gmail.com>
2025-10-31 00:29:08 +0700

    configure TypeORM with PostgreSQL connection settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    TypeOrmModule.forRoot({
+      type: 'postgres', // or 'mysql'
+      host: 'localhost',
+      port: 5432, // or 3306 for MySQL
+      username: 'postgres', // change to your local DB username
+      password: '1234', // change to your local DB password
+      database: 'testdb', // must exist in your DB
+      autoLoadEntities: true,
+      synchronize: true, // auto-create tables
+    }),
+  ],
 })
 export class AppModule {}
